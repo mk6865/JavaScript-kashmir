e29f8599fba3774518b5b214647294548e867a7c
Momin Khan <143834257+mk6865@users.noreply.github.com>
2025-07-12 07:28:47 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 13344
+let accountEmail = "mkskdfha@gmailcom"  // it can be changed after assigning the value it can be changed
+
+var accountPassword  = "1234" // it suspended now days now because there was the scope problem intially
+// please  dont use the var   it is recommmended dont use the var because the block scope and functional scope
+
+accountCity = "Kashmir"
+// const means cannot change after assigning the value in the const variable
+
+// accountId = 2 // not allowed in the javascript
+accountEmail = "hitava@gmail"
+accountPassword = "123232"
+accountCity  = "benglore"
+let accountState;
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use stict"; // treat all the Js code as newer version 
+
+// alert(2+3)  we are using nodejs. not browser
+
+// mostly we are avoiding th semicolons because the in programming the readibility
+console.log(3+3);
+console.log("MOMIN KhaN");
+
+let name = "momin" // string datatype
+let age = 23; // int datatype
+let isloggedIn  = false // boolean datatye
+
+// number => 2 to power 53  int range
+// bigInt 
+// string = "", '';
+// boolean = true, false;
+// null = its standalone value  null is the special type its empty
+// undefined = not asssinged value yet
+// symbol = uniques
+
+
+// object non_primitives datatypes
+
+console.log(typeof undefined); // output is undefined 
+console.log(typeof null) ; // output is object;
