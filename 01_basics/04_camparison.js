bb2fc246dfb0e875bd0b578322319b1a772d7953
Momin Khan <143834257+mk6865@users.noreply.github.com>
2025-07-13 05:21:30 +0000

    comparison

---
+++ 01_basics/04_camparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<5)
+// console.log(2==1)
+// console.log( 2 != 1)
+
+
+
+console.log("2" > 1)
+console.log("02" > 1);
+
+console.log(null > 0)
+console.log( null == 0)// comparsion and double equal are working diffenrently
+console.log(null >=0 )
+// we should avoid these camparison becaue it  create confusion lot of confusion
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);// all the output is false
+
+
+
+console.log("2" === 2); // strict check 
