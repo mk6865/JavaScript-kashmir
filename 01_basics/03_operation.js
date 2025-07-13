331cbc227bfcd2dc073a5e06510c33c855420eba
Momin Khan <143834257+mk6865@users.noreply.github.com>
2025-07-13 05:09:22 +0000

    operation

---
+++ 01_basics/03_operation.js
@@ -0,0 +1,39 @@
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2);
+console.log(3-2)
+console.log(3*3)
+console.log(2/3)
+
+
+let str1 = "hitesh"
+let str2 = "Momin"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log(1 + "2")
+console.log("2" + 1)
+console.log("1" + 2 + 2)
+console.log(1+2 + "2")
+
+console.log("3" + (2+3))
+
+
+
+console.log(+true);// output is the 1
+console.log(true); // output is the ture
+console.log(+"")
+
+
+let num1, num2,num3
+num1= num2 = num3 = 2+3 // not prefereed its not readible 
+console.log(num1, num2 , num3);
+
+
+let gamecounter = 100
+console.log(gamecounter)
+
+gamecounter++;
+console.log(gamecounter)
